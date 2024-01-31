ce4959530a5ffe740b14ad96f8f20001da1ecca1
norrisb5 <norrisb1@wit.edu>
2024-01-31 12:13:30 -0500

    Added More  Barn Events

--- Scenes/SummerScene.py
+++ Scenes/SummerScene.py
@@ -26,7 +26,7 @@ class summer_scene:
         destination = ""
         print("You see plains straight ahead, between you and the tower. You'll have to cross them to get to Timebro. To the East you see a gaping cave mouth. The insides are a mystery. To the far west, you can see an abandoned barn. Perhaps it can be looted.\n")
         while destination not in zones:
-            print("Where do you go?\nOPTIONS:\n1 - plains\n2 - cave\n3 - barn")
+            print("Where do you go?\n\nOPTIONS:\n1 - plains\n2 - cave\n3 - barn")
             destination = input()
             if destination is "1":
                 summer_scene.plains(summer_scene.first_plains_visit, summer_scene.knight_is_alive)
@@ -53,7 +53,7 @@ class summer_scene:
             plains_choice = ""
             plains_options = ["1","2"]
             while plains_choice not in plains_options:
-                print("What do you do?\nOPTIONS:\n1 - fight\n2 - run")
+                print("What do you do?\n\nOPTIONS:\n1 - fight\n2 - run")
                 plains_choice = input()
                 if plains_choice is "1":
                     #Program Combat Mechanics
@@ -66,7 +66,7 @@ class summer_scene:
             plains_choice = ""
             plains_options = ["1","2"]
             while plains_choice not in plains_options:
-                print("Do you proceed?\nOPTIONS:\n1 - Yes\n2 - No")
+                print("Do you proceed?\n\nOPTIONS:\n1 - Yes\n2 - No")
                 plains_choice = input()
                 if plains_choice is "1":
                     print("You march on, ready for whatever may come next.")
@@ -88,13 +88,13 @@ class summer_scene:
         cave_options = ["1","2","3"]
 
         while cave_choice not in cave_options:
-            print("What do you do?\nOPTIONS:\n1 - Explore Cave\n2 - Rummage Through Barrels")
+            print("What do you do?\n\nOPTIONS:\n1 - Explore Cave\n2 - Rummage Through Barrels")
             cave_choice = input()
             if cave_choice is "1":
                 has_lantern = False
                 #CHECK IF THEY HAVE LANTERN??
                 if has_lantern is False:
-                    print("Against your better judgement, you walk into the cave without a light to shine the way. You walk until you can't see anything around you, until eventually you hear growling. You don't even have time to react before the source of the sound eats you in one bite.\n YOU HAVE DIED")
+                    print("Against your better judgement, you walk into the cave without a light to shine the way. You walk until you can't see anything around you, until eventually you hear growling. You don't even have time to react before the source of the sound eats you in one bite.\n\n YOU HAVE DIED")
                 elif has_lantern is True:
                     summer_scene.cave_event_2()
             elif cave_choice is "2":
@@ -110,24 +110,65 @@ class summer_scene:
         cave_choice_2 = ""
         cave_options_2 = ["1","2","3"]
         while cave_choice_2 not in cave_options_2:
-            print("What do you do?\nOPTIONS:\n1 - Pull The Sword Out\n2 - Press Deeper\n3 - Turn Around")
+            print("What do you do?\n\nOPTIONS:\n1 - Pull The Sword Out\n2 - Press Deeper\n3 - Turn Around")
             cave_choice_2 = input()
             if cave_choice_2 is "1":
                 #ADD SWORD TO WEAPONS
                 print("The sword pulls from the stone with relative ease; you heft it in your hands, and it feels... right. You're unsure how, but you know the name of this sword: Rexcalibur.")
             elif cave_choice_2 is "2":
-                print("You take one step down the next tunnel, and gaping jaws clamp down on you. It wasn't a tunnel at all!\nYOU HAVE DIED")
+                print("You take one step down the next tunnel, and gaping jaws clamp down on you. It wasn't a tunnel at all!\n\nYOU HAVE DIED")
             elif cave_choice_2 is "3":
                 print("You turn around, and return to the entrance of the cave.")
                 summer_scene.cave()
 
     def barn(first_visit):
-        print("You arrive at the barn, and it looks even more worn down up close. It appears to be the only standing structure aside from the tower for miles. Miscellaneous supplies are strewn about outside, and the front door doesn't appear to be locked.")
-        
-        done = False
+        if first_visit:
+            print("You arrive at the barn, and it looks more worn down up close. Despite this, it appears to be the only standing structure aside from the tower for miles. Miscellaneous supplies are strewn about outside, and the front door doesn't appear to be locked.")
+            summer_scene.barn_event_1()
+        else:
+            print("You arrive at the barn once more. You wonder if it looks even more worn down since you were here last.")
+            summer_scene.barn_event_1()
 
     def barn_event_1():
-        done = false;
+        barn_choice = ""
+        barn_options = ["1","2","3"]
+        while barn_choice not in barn_options:
+            print("What do you do?\n\nOPTIONS:\n1 - Pull on Front Door\n2 - Knock on Front Door\n3 - Search Through Supplies")
+            barn_choice = input()
+            if barn_choice is "1":
+                print("You give the handle a pull, but it doesn't budge. Perhaps the door is barred from the inside.")
+                #tower():
+            elif barn_choice is "2":
+                print("You knock on the door, and after a long pause you eventually hear 'Who's there?!'. They sound irritated and anxious.")
+                summer_scene.intro_choices()
+            elif barn_choice is "3":
+                done = False
 
+    def barn_event_2():
+        barn_choice = ""
+        barn_options = ["1","2"]
+        while barn_choice not in barn_options:
+            print("What do you say?\n\nOPTIONS:\n1 - "+MainCharacter.name+"\n2 - Timebro")
+            barn_choice = input()
+            if barn_choice is "1":
+                print("There's another pause. 'Haven't heard that name before. You must be another survivor. Here, let me get the door for ya.' You hear a sliding sound, and then the door opens to reveal a man in ragged clothing. He gestures inside.")
+
+            elif barn_choice is "2":
+                print("Another pause, then you hear a scoff through the door. 'So that's what you're calling yourself now? Don't make yourself out to be a bigger fool than you already are, Johnny. Go away.' You take a step back and take in your surroundings again.")
+                summer_scene.barn_event_1()
+    def barn_event_3():
+        barn_choice = ""
+        barn_options = ["1","2"]
+        while barn_choice not in barn_options:
+            print("What do you do?\n\nOPTIONS:\n1 - Enter the Barn\n2 - Back Away")
+            barn_choice = input()
+            if barn_choice is "1":
+                done = False
+            elif barn_choice is "2":
+                print("'Suit yourself,' the man says, closing the door once more.")
+                summer_scene.barn_event_1()
+
+    def barn_event_4():
+        done = False
 
     run_scene(MainCharacter)
